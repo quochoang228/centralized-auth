778a1a0fdb5e6dcf43488dd365bb177b364d2928
quocht5 <quocht5@viettel.com.vn>
2025-08-04 14:08:29 +0700

    Refactor login page layout for improved readability and structure

--- lib/src/ui/page/login_page.dart
+++ lib/src/ui/page/login_page.dart
@@ -69,114 +69,112 @@ class _LoginCentralizedPageState extends ConsumerState<LoginCentralizedPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(DSSpacing.spacing6),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Đăng nhập tập trung',
+                      style: DSTextStyle.titleMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: DSSpacing.spacing4),
+                    DSTextField(
+                      controller: userController,
+                      // prefix: MyAssets.icons.user.svg(),
+                      labelText: 'Tài khoản',
+                      hintText: 'Nhập tài khoản',
+                    ),
+                    const SizedBox(height: DSSpacing.spacing4),
+                    DSTextField(
+                      controller: passWorkController,
+                      labelText: 'Mật khẩu',
+                      hintText: 'Nhập mật khẩu đăng nhập',
+                      // prefix: MyAssets.icons.unlockOtp.svg(),
+                      keyboardType: TextInputType.visiblePassword,
+                      suffix: InkWell(
+                          onTap: () {
+                            isObscureText.value = !isObscureText.value;
+                          },
+                          child: isObscureText.value
+                              ? Icon(Icons.visibility)
+                              : Icon(Icons.visibility_off)),
+                      obscureText: isObscureText.value,
+                    ),
+                    const SizedBox(height: DSSpacing.spacing4),
+                    RichText(
+                      text: TextSpan(
+                        text: "Tôi đồng ý với ",
+                        style: DSTextStyle.captionLarge
+                            .copyWith(color: DSColors.textTitle),
+                        children: [
+                          TextSpan(
+                            text: "Điều khoản dịch vụ ",
+                            style: DSTextStyle.captionLarge
+                                .copyWith(color: DSColors.info),
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                          ),
+                          TextSpan(
+                            text: "và ",
+                            style: DSTextStyle.captionLarge
+                                .copyWith(color: DSColors.textTitle),
+                          ),
+                          TextSpan(
+                            text: "chính sách bảo mật ",
+                            style: DSTextStyle.captionLarge
+                                .copyWith(color: DSColors.info),
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                          ),
+                          TextSpan(
+                            text: "của Viettel Construction ",
+                            style: DSTextStyle.captionLarge
+                                .copyWith(color: DSColors.textTitle),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: DSSpacing.spacing4),
+                    DSButton(
+                      label: 'Đăng nhập',
+                      onPressed: isUserControllerNotEmpty &&
+                              isPassWorkControllerNotEmpty
+                          ? () {
+                              ref.read(loginProvider.notifier).login(
+                                    userName: userController.text,
+                                    psw: passWorkController.text,
+                                  );
+                            }
+                          : null,
+                    ),
+                    const SizedBox(height: DSSpacing.spacing4),
+                  ],
+                ),
+              ),
+          
+              // const Spacer(),
+          
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: "Bạn chưa có tài khoản? ",
+                    style: DSTextStyle.captionLarge
+                        .copyWith(color: DSColors.textTitle),
                     children: [
-                      Text(
-                        'Đăng nhập tập trung',
-                        style: DSTextStyle.titleMedium,
-                        textAlign: TextAlign.center,
+                      TextSpan(
+                        text: "Đăng ký ngay",
+                        style: DSTextStyle.captionLarge
+                            .copyWith(color: DSColors.primary),
+                        recognizer: TapGestureRecognizer()..onTap = () {},
                       ),
-                      const SizedBox(height: DSSpacing.spacing4),
-                      DSTextField(
-                        controller: userController,
-                        // prefix: MyAssets.icons.user.svg(),
-                        labelText: 'Tài khoản',
-                        hintText: 'Nhập tài khoản',
-                      ),
-                      const SizedBox(height: DSSpacing.spacing4),
-                      DSTextField(
-                        controller: passWorkController,
-                        labelText: 'Mật khẩu',
-                        hintText: 'Nhập mật khẩu đăng nhập',
-                        // prefix: MyAssets.icons.unlockOtp.svg(),
-                        keyboardType: TextInputType.visiblePassword,
-                        suffix: InkWell(
-                            onTap: () {
-                              isObscureText.value = !isObscureText.value;
-                            },
-                            child: isObscureText.value
-                                ? Icon(Icons.visibility)
-                                : Icon(Icons.visibility_off)),
-                        obscureText: isObscureText.value,
-                      ),
-                      const SizedBox(height: DSSpacing.spacing4),
-                      RichText(
-                        text: TextSpan(
-                          text: "Tôi đồng ý với ",
-                          style: DSTextStyle.captionLarge
-                              .copyWith(color: DSColors.textTitle),
-                          children: [
-                            TextSpan(
-                              text: "Điều khoản dịch vụ ",
-                              style: DSTextStyle.captionLarge
-                                  .copyWith(color: DSColors.info),
-                              recognizer: TapGestureRecognizer()..onTap = () {},
-                            ),
-                            TextSpan(
-                              text: "và ",
-                              style: DSTextStyle.captionLarge
-                                  .copyWith(color: DSColors.textTitle),
-                            ),
-                            TextSpan(
-                              text: "chính sách bảo mật ",
-                              style: DSTextStyle.captionLarge
-                                  .copyWith(color: DSColors.info),
-                              recognizer: TapGestureRecognizer()..onTap = () {},
-                            ),
-                            TextSpan(
-                              text: "của Viettel Construction ",
-                              style: DSTextStyle.captionLarge
-                                  .copyWith(color: DSColors.textTitle),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: DSSpacing.spacing4),
-                      DSButton(
-                        label: 'Đăng nhập',
-                        onPressed: isUserControllerNotEmpty &&
-                                isPassWorkControllerNotEmpty
-                            ? () {
-                                ref.read(loginProvider.notifier).login(
-                                      userName: userController.text,
-                                      psw: passWorkController.text,
-                                    );
-                              }
-                            : null,
-                      ),
-                      const SizedBox(height: DSSpacing.spacing4),
                     ],
                   ),
                 ),
-
-                // const Spacer(),
-
-                Center(
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Bạn chưa có tài khoản? ",
-                      style: DSTextStyle.captionLarge
-                          .copyWith(color: DSColors.textTitle),
-                      children: [
-                        TextSpan(
-                          text: "Đăng ký ngay",
-                          style: DSTextStyle.captionLarge
-                              .copyWith(color: DSColors.primary),
-                          recognizer: TapGestureRecognizer()..onTap = () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
